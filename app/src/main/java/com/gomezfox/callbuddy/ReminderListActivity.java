48c10f017879cae92c9a79fcb1ad3346315879f5
gomezfox <stephen@gomezfox.net>
2019-10-24 15:45:31 -0700

    Initial commit

---
+++ app/src/main/java/com/gomezfox/callbuddy/ReminderListActivity.java
@@ -0,0 +1,14 @@
+package com.gomezfox.callbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_list);
+    }
+}
